040b0f4ccc7dbdfd13e983fcc878079d372af7ce
abdullah <abdullah.akalin@gazi.edu.tr>
2018-01-27 14:55:05 +0300

    Drawing graph of a mathematical function.

--- tools.h
+++ tools.h
@@ -3,11 +3,19 @@
 
 #define random(a) rand()%a + 1
 #define random0(a) rand()%a
+#define WIDTH 640
+#define HEIGHT 480
 
 class GLintPoint 
 {
     public:
         GLint x, y;
+};
+
+class GLdoublePoint
+{
+    public:
+        GLdouble x, y;
 };
 
 #endif
